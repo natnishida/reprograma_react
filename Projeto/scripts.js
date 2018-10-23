37c7848945d0d7f43cbc3b3077ee8071208cf39b
Natasha Nishida <nat.nishida@gmail.com>
2018-10-22 21:57:19 -0300

    Versão inicial de links com react

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,29 +1,8 @@
-//criar um link via javascript puro;
-// <a class="link" href="/conta">Criar Conta</a>
+const linkCriarUmaConta = React.createElement('a', {className: 'link', href:'/conta', children:'Criar uma Conta'})
+const fazerLogin = React.createElement('a', {className: 'link', href:'/login', children:'Fazer Login'})
 
-const linkCriarUmaConta = document.createElement('a')
-linkCriarUmaConta.className = "link"
-linkCriarUmaConta.href = "/conta"
-linkCriarUmaConta.appendChild(document.createTextNode('Criar uma Conta'))
-
-
-
-//criar um link via javascript puro
-// <a class="link" href="/login">Fazer Login</a>
-const linkFazerLogin = document.createElement('a')
-linkFazerLogin.className = "link"
-linkFazerLogin.href = "/login"
-linkFazerLogin.appendChild(document.createTextNode('Fazer Login'))
-
-
-
-//criar um formulario de login
-const formularioLogin = document.createElement('form')
-formularioLogin.className = "formLogin"
-
-formularioLogin.appendChild(linkCriarUmaConta)
-formularioLogin.appendChild(linkFazerLogin)
-
+const formularioLogin = React.createElement('form', {
+  children: [linkCriarUmaConta, fazerLogin]
+})
 const divisaoProjeto = document.getElementById('projeto')
-
-divisaoProjeto.appendChild(formularioLogin)
+ReactDOM.render(formularioLogin, divisaoProjeto)
