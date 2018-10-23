422dcc20fa6414710a6bb699e65c9bfc06da1df0
Natasha Nishida <nat.nishida@gmail.com>
2018-10-22 22:30:16 -0300

    Versão react com babel - ainda dos links

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,8 +1,20 @@
-const linkCriarUmaConta = React.createElement('a', {className: 'link', href:'/conta', children:'Criar uma Conta'})
-const fazerLogin = React.createElement('a', {className: 'link', href:'/login', children:'Fazer Login'})
+// const linkCriarUmaConta = React.createElement('a', {className: 'link', href:'/conta', children:'Criar uma Conta'});
+// const fazerLogin = React.createElement('a', {className: 'link', href:'/login', children:'Fazer Login'})
+//
+// const formularioLogin = React.createElement('form', {
+//   children: [linkCriarUmaConta, fazerLogin]
+// })
+// const divisaoProjeto = document.getElementById('projeto')
+// ReactDOM.render(formularioLogin, divisaoProjeto)
 
-const formularioLogin = React.createElement('form', {
-  children: [linkCriarUmaConta, fazerLogin]
-})
+
+const linkCriarUmaConta = <a class="link" href="/conta">Criar uma Conta</a>
+const linkFazerLogin = <a class="link" href="/login">Fazer Login</a>
+const formularioLogin = (
+<form>
+{linkCriarUmaConta}
+{linkFazerLogin}
+</form>
+)
 const divisaoProjeto = document.getElementById('projeto')
 ReactDOM.render(formularioLogin, divisaoProjeto)
