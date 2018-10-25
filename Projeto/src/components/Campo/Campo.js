b0839569e91dcc394d93c15fc61dd3b1eb43206b
Natasha Nishida <nat.nishida@gmail.com>
2018-10-24 22:28:01 -0300

    Inclusão dos componentes de campo e label

---
+++ Projeto/src/components/Campo/Campo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Campo.css'
+
+function Campo(props) {
+  return (
+    <input className="caixa-texto" type={props.type} id={props.id} name={props.name} placeholder={props.placeholder}/>
+
+  )
+}
+
+export default Campo
--- Projeto/src/paginas/Login/Login.js
+++ Projeto/src/paginas/Login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from '../../components/Link/Link'
 import Botao from '../../components/Botao/Botao'
+import Legenda from '../../components/Legenda/Legenda'
+import Campo from '../../components/Campo/Campo'
 import './Login.css'
 
 function Login() {
@@ -8,9 +10,17 @@ function Login() {
   <main className="login">
   <h1>Login</h1>
   <p>Entre com seu e-mail e Senha.</p>
-  <Link url="/conta">Criar uma Conta</Link>
+
+  <Legenda htmlFor="email">Email:</Legenda>
+  <Campo type="email" id="email" name="email" placeholder="Insira seu e-mail aqui"></Campo>
+
+  <Legenda htmlFor="senha">Senha:</Legenda>
+  <Campo type="password" id="senha" name="senha" placeholder="Insira sua senha aqui"></Campo>
+
   <Botao desabilitado texto="Enviar"></Botao>
   <Botao texto="Enviar"></Botao>
+
+  <Link url="/conta">Criar uma Conta</Link>
   </main>
 )
 };
