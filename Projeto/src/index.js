8f6229584d4921c48ca0f23e797efe4c5c672b19
Natasha Nishida <nat.nishida@gmail.com>
2018-10-25 21:02:48 -0300

    Inclusao da pasta Conta e do css e js Conta

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -4,6 +4,10 @@ import Link from './components/Link/Link'
 import Botao from './components/Botao/Botao'
 import './index.css'
 import Login from './paginas/Login/Login'
+import Conta from './paginas/Conta/Conta'
+
+
+const pagina = <Conta />
 
 const divisaoProjeto = document.getElementById('projeto')
-ReactDOM.render(Login(), divisaoProjeto)
+ReactDOM.render(pagina, divisaoProjeto)
---
+++ Projeto/src/paginas/Conta/Conta.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from '../../components/Link/Link'
+import Botao from '../../components/Botao/Botao'
+import Legenda from '../../components/Legenda/Legenda'
+import Campo from '../../components/Campo/Campo'
+import './Conta.css'
+
+function Conta() {
+  return (
+  <main className="conta">
+  <h1>Conta</h1>
+  <p>Envie o formulário para criar uma conta!</p>
+    <Legenda htmlFor="nome">Nome:</Legenda>
+    <Campo type="text" id="nome" name="nome" placeholder="Insira seu nome aqui"></Campo>
+
+    <Legenda htmlFor="telefone">Nome:</Legenda>
+    <Campo type="text" id="telefone" name="telefone" placeholder="Insira seu telefone aqui"></Campo>
+
+    <Legenda htmlFor="email">Email:</Legenda>
+    <Campo type="email" id="email" name="email" placeholder="Insira seu e-mail aqui"></Campo>
+
+    <Legenda htmlFor="senha">Senha:</Legenda>
+    <Campo type="password" id="senha" name="senha" placeholder="Insira sua senha aqui"></Campo>
+
+  <Botao desabilitado texto="Enviar"></Botao>
+  <Botao texto="Enviar"></Botao>
+
+  <Link url="/conta">Fazer Login</Link>
+  </main>
+)
+};
+export default Conta
