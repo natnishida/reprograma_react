2ce323861bd8ae962045a29b6165fe63f5300e45
Natasha Nishida <nat.nishida@gmail.com>
2018-11-07 22:22:59 -0200

    inclusão do estado carregando da home.

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,17 +1,35 @@
 import React , { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import carregando from './carregando.svg'
 import './Home.css'
 
-function Home(props) {
-  if (!props.usuario) {
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      carregando:true
+    }
+  }
+
+
+render() {
+  if (!this.props.usuario) {
     return <Redirect to="/login" />
   }
   return (
     <main className="home">
-
+      {this.state.carregando ? (
+        <img className="home__loading" src={ carregando } alt="Carregando" />
+      ) : (
+        <div>
+          Aqui listarei os post-its amanhã.
+        </div>
+      )}
     </main>
   )
+}
+
 }
 
 export default connect((state) => ({usuario: state.usuario}))(Home)
